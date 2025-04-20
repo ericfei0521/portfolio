27546f82718fad1e41dd0db5404279f3add40f7e
ericfei306 <eric0521bigjohn@gmail.com>
2025-04-20 21:51:13 +0800

    remove prisma but using just google sheet

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,24 +1,43 @@
-import prisma from '@/lib/prisma';
+import { google } from 'googleapis';
 import { NextRequest, NextResponse } from 'next/server';
+
+const SHEET_ID = '13dxOnDbg32HB06W2OT4QqkImiOPJBdwbnsiizZz8IMo';
+const RANGE = 'Sheet1!A:C'; // Adjust the range as per your sheet structure
+
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+        // Replace with your service account credentials
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
 
 export const POST = async (req: NextRequest) => {
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
-        return NextResponse.json({ message: 'Name, email, and password are required.' }, { status: 400 });
+        return NextResponse.json({ message: 'Name, email, and message are required.' }, { status: 400 });
     }
 
     try {
-        await prisma.message.create({
-            data: {
-                name,
-                email,
-                message,
+        // Append to Google Sheet
+        const sheets = google.sheets({
+            version: 'v4',
+            auth: auth,
+        });
+        await sheets.spreadsheets.values.append({
+            spreadsheetId: SHEET_ID,
+            range: RANGE,
+            valueInputOption: 'RAW',
+            requestBody: {
+                values: [[name, email, message]],
             },
         });
+
         return NextResponse.json({ message: 'Send successfully' }, { status: 200 });
     } catch (error) {
-        console.error('Error saving message:', error);
+        console.error('Error:', error);
         return NextResponse.json({ message: 'Internal server error.' }, { status: 500 });
     }
 };
